8c93e416d8e44a1d11575c3c7a6985b1f5d2ef0f
MonishaPechimuthu <72312201+MonishaPechimuthu@users.noreply.github.com>
2020-10-03 21:34:25 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <body>
+  
+  <script>
+  var tech = function(){
+  console.log('GUVI Geek Technologies, Chennai');
+  alert('GUVI Geek Technologies, Chennai');
+};
+
+tech();
+</script>
+  </body>
+  </html>
