82fe5babca87fed455f071a4b85b0dbfa3a2f02e
MonishaPechimuthu <72312201+MonishaPechimuthu@users.noreply.github.com>
2020-10-04 20:08:12 +0530

    Create farenheit.js

---
+++ farenheit.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+var temp;
+function temperature(celcius)
+{
+temp = celcius * 9 / 5 + 32;
+console.log(temp);
+}
+temperature(30);
+</script>
+</body>
+</html>
