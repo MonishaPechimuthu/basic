29b2aa4c53a46cf9342978d8daa7e4c72162623f
MonishaPechimuthu <72312201+MonishaPechimuthu@users.noreply.github.com>
2020-10-04 21:09:15 +0530

    Update farenheit.js

--- farenheit.js
+++ farenheit.js
@@ -1,14 +1,20 @@
-<!DOCTYPE html>
-<html>
-<body>
-<script>
+var readline = require('readline');
+var inp = readline.createInterface({
+input: process.stdin
+ });
+const userInput = [];
+inp.on("line", function()  {
+userInput.push(data);
+});
+inp.on("close", function()  {
+console.log(temperature(data)
 var temp;
-function temperature(celcius)
+function temperature(data)
 {
-temp = celcius * 9 / 5 + 32;
-console.log(temp);
+  
+temp = data * (9 / 5) + 32;
+var faren = parsefloat(temp.toFixed(2)); 
+console.log(temp.toFixed(2));
 }
-temperature(30);
-</script>
-</body>
-</html>
+return faren;
+}
